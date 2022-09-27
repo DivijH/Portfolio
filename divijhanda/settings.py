a7aeb241a15e237239f68962c88f1cf7ed3d0979
Divij Handa <divij.handa.2010@gmail.com>
2022-09-26 18:18:53 -0700

    Update settings.py

--- divijhanda/settings.py
+++ divijhanda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s(zu5u_l&g(ulw@q9b5ao#n01e^03i5^2ey5mh84ru+16yg&^$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
